3810bb75e23c6c71b880edbc97b11f7d972f18c7
林晓 <oncwnuAx68uilDIOfrhor-XMMMJw@git.weixin.qq.com>
2020-09-17 00:19:07 +0800

    初步测试

--- renderer.js
+++ renderer.js
@@ -4,3 +4,7 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "段落被更改。";
+}
